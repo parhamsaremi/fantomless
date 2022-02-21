e9dc2c4cd0ee9ed7f7c43c7d55ad6a134fc08a2a
ijanus <emekamicro@gmail.com>
2022-02-21 13:21:23 +0100

    Remove parenthesis when using raise keyword
    Fixes https://github.com/nblockchain/fantomless/issues/1

--- src/Fantomas.Tests/RemoveUnnecessaryParenthesesTests.fs
+++ src/Fantomas.Tests/RemoveUnnecessaryParenthesesTests.fs
@@ -369,3 +369,105 @@ module Foo =
 module Foo =
     let sum (a: int) (b: int) = a + b
 """
+
+
+[<Test>]
+let ``parentheses in function call should be removed`` () =
+    formatSourceString
+        false
+        """
+raise(InvalidPassword)
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+raise InvalidPassword
+"""
+
+[<Test>]
+let ``nothing should be changed`` () =
+    formatSourceString
+        false
+        """
+raise InvalidPassword
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+raise InvalidPassword
+"""
+
+[<Test>]
+let ``parentheses should be removed and left pipe introduced between arguments and function call`` () =
+    formatSourceString
+        false
+        """
+raise(AddressWithInvalidChecksum None)
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+raise <| AddressWithInvalidChecksum None
+"""
+
+[<Test>]
+let ``parentheses should be removed and left pipe introduced between argument(inner function call)  and function call``
+    ()
+    =
+    formatSourceString
+        false
+        """
+raise(Exception("foo"))
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+raise <| Exception("foo")
+"""
+
+[<Test>]
+let ``parentheses should be removed and left pipe introduced between argument(inner function call with two arguments)  and function call``
+    ()
+    =
+    formatSourceString
+        false
+        """
+raise(Exception(ex.ToString(), (ex.InnerException)))
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+raise <| Exception(ex.ToString(), (ex.InnerException))
+"""
+
+[<Test>]
+let ``using the new keyword`` () =
+    formatSourceString
+        false
+        """
+let CalculateSum (file: FileInfo) =
+    if not (file.Exists) then
+        raise (new FileNotFoundException("File not found", file.FullName))
+    ()
+"""
+        config
+    |> prepend newline
+    |> should
+        equal
+        """
+let CalculateSum (file: FileInfo) =
+    if not (file.Exists) then
+        raise <| new FileNotFoundException("File not found", file.FullName)
+
+    ()
+"""
